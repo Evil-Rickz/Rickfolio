81f7e605c2e52d336d7e80cb1cddf94330eab353
Evil-Rickz <ricklimafs@hotmail.com>
2022-06-22 19:40:36 -0300

    Home and About section add

---
+++ JS/app.js
@@ -0,0 +1,31 @@
+/*------------------------------ MENU SHOW/HIDDEN Y ------------------------------*/
+const navMenu = document.getElementById ('nav-menu'),
+        navToggle = document.getElementById ('nav-toggle'),
+        navClose = document.getElementById ('nav-close')
+
+/*------------------------------ SHOW MENU ------------------------------*/
+/* Validate if contant exists */
+if (navToggle) {
+    navToggle.addEventListener ('click', () =>{
+        navMenu.classList.add ('show-menu')
+    })
+}
+
+/*------------------------------ HIDDEN MENU ------------------------------*/
+/* Validate if contant exists */
+if (navClose){
+    navClose.addEventListener ('click', () =>{
+        navMenu.classList.remove ('show-menu')
+    })
+ }
+
+/*------------------------------ REMOVE MENU MOBILE ------------------------------*/
+const navLink = document.querySelectorAll ('.nav_link')
+
+function linkAction (){
+    const navMenu = document.getElementById ('nav-menu')
+    //when we click on each 'nav_link, we remove the show-menu class.
+    navMenu.classList.remove ('show-menu')
+}
+
+navLink.forEach (n => n.addEventListener ('click', linkAction))
