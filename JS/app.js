db30b278a1219950ec9921afa8a37d0b513729c7
Evil-Rickz <ricklimafs@hotmail.com>
2022-07-10 13:55:55 -0300

    Qualification section has added

--- JS/app.js
+++ JS/app.js
@@ -41,5 +41,12 @@ tabs.forEach(tab =>{
         tabContents.forEach(tabContent =>{
             tabContent.classList.remove('qualification_active')
         })
+        target.classList.add('qualification_active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification_active')
+        })
+
+        tab.classList.add('qualification_active')
     })
 })        
