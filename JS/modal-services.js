48d69275514477dfa336677525cd5b9ce7e8efc0
Evil-Rickz <ricklimafs@hotmail.com>
2022-09-28 09:51:00 -0300

    Modal screen in services section

---
+++ JS/modal-services.js
@@ -0,0 +1,21 @@
+/*------------------------------ SERVICES MODAL ------------------------------*/
+function openModal(modalID) {
+    const modal = document.getElementById(modalID);
+    if (modal) {
+//        modal.classList.toggle('active-modal');
+/*        modal.addEventListener('click', (e) => {
+            if(e.target.id == modalID || e.target.className == 'modal-close') {
+                modal.classList.remove('active-modal')
+            }
+        })*/
+    }
+};
+
+const branding = document.querySelector('.branding');
+branding.addEventListener('click', () => openModal('branding-modal'))
+
+const flyers = document.querySelector('.flyers');
+flyers.addEventListener('click', () => openModal('flyers-modal'))
+
+const motion = document.querySelector('.motion');
+motion.addEventListener('click', () => openModal('motion-modal'))
